f78fb2a00efc899391ae78a0f5e2653c15a8576f
Marcos Afonso Vieira <marcos.vieira@pelicano.br>
2026-01-14 09:09:47 -0300

    remove prisma

--- prisma/migrations/20260113193037_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Produto" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "nome" TEXT NOT NULL,
-    "preco" REAL NOT NULL,
-    "estoque" REAL NOT NULL,
-    "categoria" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
